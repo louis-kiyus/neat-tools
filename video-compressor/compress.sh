9e7d8c35eef56e945026a87c366b2369974712a0
louis-kiyus <75559366+louis-kiyus@users.noreply.github.com>
2022-12-19 14:44:07 -0500

    added video compressor script
    compresses all the mp4 files in a directory

---
+++ video-compressor/compress.sh
@@ -0,0 +1,12 @@
+quality=36
+while getopts q: flag
+do
+  case "${flag}" in
+    q) quality=${OPTARG};;
+  esac
+done
+for file in ./*.mp4
+do
+  ffmpeg -i $file -vcodec libx265 -crf $quality ${file%\.*}-compressed.mp4
+done
+
