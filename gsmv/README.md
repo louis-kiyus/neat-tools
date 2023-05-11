41fd35d7e0c96a977cd7be06f48092d9508d078f
louis-kiyus <75559366+louis-kiyus@users.noreply.github.com>
2023-05-10 23:54:39 -0400

    Added multi-file support
    You can now pass in an infinite number of files at once. You can also specify the line number in the PDF to be used as the filename with the -i flag

--- gsmv/README.md
+++ gsmv/README.md
@@ -2,7 +2,7 @@
 
 ## Abstract
 
-This script renames a PDF file specified in its first argument by extracting the title from the PDF's first line and using it as the new filename. The script has been tested with PDFs that are graded on Gradescope.
+This script renames PDF file(s) by extracting the title from the PDF's first line (modifiable by the `-i` flag) and using it as the new filename. The script has been specifically tested with PDFs that are graded on Gradescope and can be used on any other PDFs.
 
 ## Instructions
 
@@ -13,6 +13,18 @@ python3 pdf-rename.py "full-path-here-enclosed-with-quotation-mark.pdf"
 ```
 
 As an example, here's an example of invoking a script named `submission-13.pdf`: `python3 pdf-rename.py "~/Documents/submission-13.pdf"`. If you're on a Mac, you can simply drag-and-drop the file into your Terminal.
+
+You can include an infinite number of PDFs in the command. For example:
+
+```
+python3 pdf-rename.py "pdf_1.pdf" "pdf_2.pdf"
+```
+
+## Additional flag
+
+| Flag   | Description                                                                                                                                 |
+| ------ | ------------------------------------------------------------------------------------------------------------------------------------------- |
+| `-i` | This flag is used to specify the line number in the PDF that will be used as the filename. By default, we will use the first line (line 0). |
 
 ## Aliasing gsmv
 
--- gsmv/pdf-rename.py
+++ gsmv/pdf-rename.py
@@ -1,24 +1,34 @@
 import PyPDF2
 import os
 import sys
+import argparse
 
-# Select all pdf files in the current directory
+parser = argparse.ArgumentParser(description='Process some arguments')
+# the * means that there can be any number of arguments
+parser.add_argument('filepaths', metavar='filepath',
+                    type=str, nargs='*', help='input file\'s paths')
+parser.add_argument('-i', '--index', type=int, default=0,
+                    help='Line number in PDF to be used as filename')
+args = parser.parse_args()
 
-original_dir = sys.argv[1]
+if len(args.filepaths) == 0:
+    print("No filepaths provided")
+    sys.exit(1)
+for original_dir in args.filepaths:
+    print("Filepath:", original_dir)
 
-print("Original directory:", original_dir)
-dir_parts = original_dir.split("/")
-# excludes the last element, which is the original filename
-# the [''] at the end is to add a trailing slash (since we need to add the filename later)
-# -> works because we're adding an empty string to the list
-new_dir = "/".join(dir_parts[:-1] + [''])
+    dir_parts = original_dir.split("/")
+    # excludes the last element, which is the original filename
+    # the [''] at the end is to add a trailing slash (since we need to add the filename later)
+    # -> works because we're adding an empty string to the list
+    new_dir = "/".join(dir_parts[:-1] + [''])
 
-pdfFileObj = open(original_dir, 'rb')
-pdfReader = PyPDF2.PdfReader(pdfFileObj)
-pageObj = pdfReader.pages[0]
-new_filename = pageObj.extract_text().splitlines()[0] + ".pdf"
-new_dir += new_filename
-print()
-print("New Filename:", new_filename)
-os.system("mv " + "\"" + original_dir +
-          "\"" + " " + "\"" + new_dir + "\"")
+    pdfFileObj = open(original_dir, 'rb')
+    pdfReader = PyPDF2.PdfReader(pdfFileObj)
+    pageObj = pdfReader.pages[0]
+    new_filename = pageObj.extract_text().splitlines()[args.index] + ".pdf"
+    new_dir += new_filename
+    print()
+    print("New Filename:", new_filename)
+    os.system("mv " + "\"" + original_dir +
+              "\"" + " " + "\"" + new_dir + "\"")
